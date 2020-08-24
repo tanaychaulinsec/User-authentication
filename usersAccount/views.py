6a03da4aae7504b7408bf61677281d762636bd6a
tanaychaulinsec <tanaychauli@gmail.com>
2020-08-24 20:50:57 +0530

    in views.py write the func for user_login , user_logout and profile

--- usersAccount/views.py
+++ usersAccount/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-from usersAccount.forms import UserForm,UserProfileInfoForm
+from django.shortcuts import render,redirect
+from usersAccount.forms import UserForm, UserProfileInfoForm
+
+from django.contrib.auth import authenticate,login,logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
 def index(request):
-    return render(request,"usersAccount/index.html")
+    return render(request, "usersAccount/index.html")
+
+@login_required
+def profile(request):
+    return render(request,"usersAccount/profile.html")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('index')
 
 def signup(request):
     
@@ -30,4 +46,35 @@ def signup(request):
         user_form = UserForm
         profile_form = UserProfileInfoForm
         
-    return render(request,"usersAccount/signup.html",{'profile_form':profile_form,'user_form':user_form,'registered':registered})
+    return render(request, "usersAccount/signup.html", {'profile_form': profile_form, 'user_form': user_form, 'registered': registered})
+
+def user_login(request):
+
+    if request.method == 'POST':
+        # First get the username and password supplied
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Django's built-in authentication function:
+        user = authenticate(username=username, password=password)
+
+        # If we have a user
+        if user:
+            #Check it the account is active
+            if user.is_active:
+                # Log the user in.
+                login(request,user)
+                # Send the user back to some page.
+                # In this case their homepage.
+                return redirect('profile')
+            else:
+                # If account is not active:
+                return HttpResponse("Your account is not active.")
+        else:
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username,password))
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        #Nothing has been provided for username or password.
+        return render(request, 'usersAccount/login.html', {})
